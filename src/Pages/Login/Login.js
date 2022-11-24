3910907e3ef6749a8fbc1ea12a4ee2a63026c69e
shahadot99999 <shahadot99999@gmail.com>
2022-11-25 00:08:45 +0600

    Google sign

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,3 +1,4 @@
+import { GoogleAuthProvider } from 'firebase/auth';
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -6,12 +7,23 @@ import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const { signIn } = useContext(AuthContext);
+    const { signIn, providerLogin } = useContext(AuthContext);
     const [loginError, setLoginError] = useState('');
    const location = useLocation();
     const navigate = useNavigate();
 
    const from = location.state?.from?.pathname || '/';
+
+   const googleProvider = new GoogleAuthProvider()
+
+   const handleGoogleSignIn=()=>{
+    providerLogin(googleProvider)
+    .then(result=>{
+        const user = result.user;
+        console.log(user)
+    })
+    .catch(error=>console.error(error))
+   }
 
     const handleLogin = data => {
         console.log(data);
@@ -60,7 +72,7 @@ const Login = () => {
                 </form>
                 <p>Go to resale mobile <Link className='text-white' to="/signup">Create new Account</Link></p>
                 <div className="divider">OR</div>
-                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                <button onClick={handleGoogleSignIn} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
             </div>
         </div>
     );
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -16,6 +16,10 @@ const AuthProvider = ({children}) => {
     const signIn = (email, password) =>{
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const providerLogin = (provider)=>{
+        return signInWithPopup(auth, provider)
     }
 
     const updateUser = (userInfo) =>{
@@ -40,6 +44,7 @@ const AuthProvider = ({children}) => {
     const authInfo = {
         createUser,
         signIn,
+        providerLogin,
         updateUser,
         logOut,
         user,
