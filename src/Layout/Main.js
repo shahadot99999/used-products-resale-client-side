1e709f826d6fb1ae35d80516b678bf1d69bb7428
shahadot99999 <shahadot99999@gmail.com>
2022-11-24 09:25:21 +0600

    data collection different websites

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Outstanding from '../Pages/Shared/Outstanding/Outstanding';
 
 const Main = () => {
     return (
         <div>
           <Navbar></Navbar>
           <Outlet></Outlet>
+          <Outstanding></Outstanding>
           <Footer></Footer>
         </div>
     );
---
+++ src/Pages/Shared/Outstanding/Outstanding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Outstanding = () => {
+    return (
+        <div className= ''>
+        <h2 className='  text-3xl text-center text-fuchsia-900 bg-orange-500'>Sell Your Resale Product</h2>
+
+        </div>
+    );
+};
+
+export default Outstanding;
